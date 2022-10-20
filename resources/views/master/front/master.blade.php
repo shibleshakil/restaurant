8b62bbd17c6385331962cdb945a1121aba54bcd4
shibleshakil <shakil.shible@gmail.com>
2022-10-20 17:02:25 +0600

    footer

--- resources/views/master/front/master.blade.php
+++ resources/views/master/front/master.blade.php
@@ -81,19 +81,19 @@
                         <h3>EAT</h3>
                         <ul class="list-group">
                             <li>
-                                <a href="" class="col-green">Book a Table</a>
+                                <a href="#" class="col-green">Book a Table</a>
                             </li>
                             <li>
-                                <a href="" class="col-green">Menus</a>
+                                <a href="#" class="col-green">Menus</a>
                             </li>
                             <li>
-                                <a href="" class="col-green">Allergens</a>
+                                <a href="#" class="col-green">Allergens</a>
                             </li>
                             <li>
-                                <a href="" class="col-green">Restaurants</a>
+                                <a href="#" class="col-green">Restaurants</a>
                             </li>
                             <li>
-                                <a href="" class="col-green">Take Away</a>
+                                <a href="#" class="col-green">Take Away</a>
                             </li>
                         </ul>
                     </div>
@@ -101,19 +101,19 @@
                         <h3>ENJOY</h3>
                         <ul class="list-group">
                             <li>
-                                <a href="" class="col-green">Lunch menu</a>
+                                <a href="#" class="col-green">Lunch menu</a>
                             </li>
                             <li>
-                                <a href="" class="col-green">Cinema Club</a>
+                                <a href="#" class="col-green">Cinema Club</a>
                             </li>
                             <li>
-                                <a href="" class="col-green">Gift Vouchers</a>
+                                <a href="#" class="col-green">Gift Vouchers</a>
                             </li>
                             <li>
-                                <a href="" class="col-green">Party Booking</a>
+                                <a href="#" class="col-green">Party Booking</a>
                             </li>
                             <li>
-                                <a href="" class="col-green">Christmas</a>
+                                <a href="#" class="col-green">Christmas</a>
                             </li>
                         </ul>
                     </div>
@@ -121,16 +121,16 @@
                         <h3>US</h3>
                         <ul class="list-group">
                             <li>
-                                <a href="" class="col-green">About</a>
+                                <a href="#" class="col-green">About</a>
                             </li>
                             <li>
-                                <a href="" class="col-green">Careers</a>
+                                <a href="#" class="col-green">Careers</a>
                             </li>
                             <li>
-                                <a href="" class="col-green">Mystery Diners</a>
+                                <a href="#" class="col-green">Mystery Diners</a>
                             </li>
                             <li>
-                                <a href="" class="col-green">News & Events</a>
+                                <a href="#" class="col-green">News & Events</a>
                             </li>
                         </ul>
                     </div>
@@ -138,35 +138,35 @@
                         <h3>HELP</h3>
                         <ul class="list-group">
                             <li>
-                                <a href="" class="col-green">Feedback</a>
+                                <a href="#" class="col-green">Feedback</a>
                             </li>
                             <li>
-                                <a href="" class="col-green">Cancel Booking</a>
+                                <a href="#" class="col-green">Cancel Booking</a>
                             </li>
                             <li>
-                                <a href="" class="col-green">Help & Advice</a>
+                                <a href="#" class="col-green">Help & Advice</a>
                             </li>
                             <li>
-                                <a href="" class="col-green">Contact</a>
+                                <a href="#" class="col-green">Contact</a>
                             </li>
                             <li>
-                                <a href="" class="col-green">Frequently Asked Question</a>
+                                <a href="#" class="col-green">Frequently Asked Question</a>
                             </li>
                         </ul>
                     </div>
                 </div>
             </div>
             <div class="icons p-4 text-center fs-3">
-                <a href="" class="">
+                <a href="#" class="">
                     <span><i class="fab fa-facebook col-green mx-3"></i></span>
                 </a>
-                <a href="">
+                <a href="#">
                     <span><i class="fab fa-whatsapp col-green mx-3"></i></span>
                 </a>
-                <a href="">
+                <a href="#">
                     <span><i class="fab fa-instagram col-green mx-3"></i></span>
                 </a>
-                <a href="">
+                <a href="#">
                     <span><i class="fab fa-twitter col-green mx-3"></i></span>
 
                 </a>
@@ -182,20 +182,20 @@
                 <div class="col-md-6">
                     <div class="row">
                         <div class="col-md-4">
-                            <a href="" class="col-green">© 2022 Wildwood</a>
+                            <a href="#" class="col-green">© {{date('Y')}} Wildwood</a>
                         </div>
                         <div class="col-md-4">
-                            <a href="" class="col-green">Terms & Conditions</a>
+                            <a href="#" class="col-green">Terms & Conditions</a>
                         </div>
                         <div class="col-md-4">
-                            <a href="" class="col-green">Privacy Policy</a>
+                            <a href="#" class="col-green">Privacy Policy</a>
                         </div>
                     </div>
 
                 </div>
                 <div class="col-md-6">
                     <div class="text-end">
-                        <a href="" class="col-green">Website By <span class="fw-bold">NextWave</span> </a>
+                        <!-- <a href="" class="col-green">Website By <span class="fw-bold">NextWave</span> </a> -->
                     </div>
 
                 </div>
