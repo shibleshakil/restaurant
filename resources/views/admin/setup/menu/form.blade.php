b037fb324a2250915a0e5ccc02c0862325a5ea61
nobijoy <nobijoy1446@gmail.com>
2022-11-14 11:51:44 +0600

    restaurant wise menu

--- resources/views/admin/setup/menu/form.blade.php
+++ resources/views/admin/setup/menu/form.blade.php
@@ -1,5 +1,16 @@
 <div class="row">
     <div class="form-group col-md-6 ">
+        <label for="restaurant_id">Select Restaurant  <span class="text-danger">*</span></label>
+        <select name="restaurant_id" id="restaurant_id" class="form-control select2" required>
+            <option value="" selected>Select</option>
+            {{-- @foreach ($restaurants as $type) --}}
+                {{-- <option value="{{$type->id}}" @if(($url == 'systemUser.edit') && $data->restaurant_id == $type->id) selected @endif>{{$type->name}} ({{$type->area->title}})</option> --}}
+                <option value="1"  >Pan Pacific</option>
+                <option value="1"  >Westin</option>
+            {{-- @endforeach --}}
+        </select>
+    </div>
+    <div class="form-group col-md-6 ">
         <label for="category_id">Select Menu Category</label>
         <select name="category_id" id="category_id" class="form-control select2">
             <option value="">Select</option>
